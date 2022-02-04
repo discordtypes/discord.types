97f008cf3c4961e061ce7a24179f192cda6d43ac
Mattéo <65495386+Nerzoxxx-dev@users.noreply.github.com>
2022-02-04 22:47:23 +0100

    Delete tests directory

--- tests/index.ts
+++
@@ -1,3 +0,0 @@
-import { toMajorRoute } from "../packages/typescord-api-structures/v9/Routes";
-
-console.log(toMajorRoute('/channels/886631972233949286'))
--- tests/test.ts
+++
@@ -1,7 +0,0 @@
-import { Rest } from "../packages/rest";
-
-var r = new Rest({})
-r.setToken('faketoken')
-r.request({method: 'GET', fullroute: '/gateway/bot'}).then(async rs => {
-  console.log(await rs.json())
-})
