e06450b2816549c0eb37ee18da11f4996f3c8f14
Mattéo <65495386+Nerzoxxx-dev@users.noreply.github.com>
2022-02-04 22:51:45 +0100

    Delete Events.d.ts

--- packages/rest/Events.d.ts
+++
@@ -1,8 +0,0 @@
-/** Rate limit event name */
-export declare const RATE_LIMIT = "rate_limit";
-/** Debuging requests */
-export declare const REQUEST = "requests";
-/** When a sweep expected */
-export declare const SWEEP = "sweep";
-/** Simple debug */
-export declare const DEBUG = "debug";
